1b1e6c5aca4b8c353d338c9d7de3df18fc79f539
tuhindewan <tuhinsshadow@gmail.com>
2017-02-10 07:02:00 -0800

    first commit

---
+++ admin/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dashboard - Sample Blog PHP</title>
+	<link rel="stylesheet" type="text/css" href="../style-admin.css">
+</head>
+<body>
+
+<div class="wrapper">
+	<div class="header">
+		<h1>Admin Panel Dashboard</h1>
+	</div>
+	<div class="container">
+		<div class="sidebar">
+			<h2>Page Options</h2>
+			<ul>
+				<li><a href="">Home</a></li>
+				<li><a href="">Change Footer Text</a></li>
+				<li><a href="">Logout</a></li>
+			</ul>
+
+			<h2>Blog Options</h2>
+			<ul>
+				<li><a href="">Add Post</a></li>
+				<li><a href="">View Post</a></li>
+				<li><a href=""></a>Manage Catagory</li>
+				<li><a href=""></a>Manage tags</li>
+			</ul>
+		</div>
+		<div class="content">
+			<h2> Welcome To Admin Panel</h2>
+			<div style="font-weight: bold;color: #3d9ccd; font-size: 28px;text-align: center;">
+				Welcome to the dashboard <br>
+				Sample Blog With PHP
+
+			</div>
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
---
+++ admin/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login - Sample Blog PHP</title>
+	<link rel="stylesheet"  href="../style-admin.css">
+</head>
+<body>
+
+<div class="wrapper_login">
+<h1>Admin Login</h1>
+<form>
+	<table>
+		<tr>
+			<td>Username:</td>
+			<td><input type="text" name="username"></td>
+		</tr>
+		<tr>
+			<td>Password:</td>
+			<td><input type="text" name="password"></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td><input type="submit" name="submit" value="LOGIN"></td>
+		</tr>
+	</table>
+</form>
+</div>
+</body>
+</html>
